ac6102c1a6a17e74ce15301c9f8a6e5fdcfca30c
Humoyun Abduqodirov <abduodirovhumoyun@gmail.com>
2024-12-21 15:07:12 +0500

    fix: merged all the changes

--- tohirjon/tohirjon.js
+++ tohirjon/tohirjon.js
@@ -5,12 +5,14 @@ const saveBtn = document.getElementById("save-btn");
 
 const carId = new URLSearchParams(window.location.search).get("id");
 
-axios.get(`https://6765c25c410f84999656159d.mockapi.io/cars/${carId}`).then((response) => {
-  const car = response.data;
-  brand.value = car.brand;
-  model.value = car.model;
-  price.value = car.price;
-});
+axios
+  .get(`https://6765c25c410f84999656159d.mockapi.io/cars/${carId}`)
+  .then((response) => {
+    const car = response.data;
+    brand.value = car.brand;
+    model.value = car.model;
+    price.value = car.price;
+  });
 
 saveBtn.addEventListener("click", () => {
   const editedCar = {
@@ -23,7 +25,7 @@ saveBtn.addEventListener("click", () => {
     .then((response) => {
       console.log(response);
       if (condition.statusCode === 200) {
-        // window.location.href = "index.html";
+        window.location.href = "../mirkomil/index.html";
       }
     })
     .catch((err) => {
